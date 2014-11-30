6c28e53baa000e69dcd4a22c7c0b7e5a336d1d58
3onyc <3onyc@x3tech.com>
2014-11-30 21:34:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# docker-link-tool
+
+Tool for linking docker containers without using explicit links, primarily useful with --icc=false
+
+## Examples
+
+### Add rule allowing container foo to access port 80 on container bar
+
+```bash
+link-tool -action add -cname foo -sname bar -p 80
+```
+
+### Remove rule allowing foo to access port 80 on bar
+
+```bash
+link-tool -action delete -cname foo -sname bar -p 80
+```
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	docker "github.com/fsouza/go-dockerclient"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	DockerUrl  *string = flag.String("d", "unix:///var/run/docker.sock", "Docker URL")
+	ClientIp   *string = flag.String("cip", "", "Client IP (Optional)")
+	ServerIp   *string = flag.String("sip", "", "Server IP (Optional)")
+	ClientName *string = flag.String("cname", "", "Client Name (Optional)")
+	ServerName *string = flag.String("sname", "", "Server Name (Optional)")
+	Port       *uint   = flag.Uint("p", 0, "Server port")
+	Action     *string = flag.String("action", "", "delete/add")
+
+	Docker *docker.Client
+)
+
+func main() {
+	flag.Parse()
+
+	if *ClientIp == "" && *ClientName == "" {
+		fmt.Println("Need one of cname or cip")
+		return
+	}
+	if *ServerIp == "" && *ServerName == "" {
+		fmt.Println("Need one of sname or sip")
+		return
+	}
+
+	if *ClientName != "" && *ClientIp == "" {
+		var err error
+		*ClientIp, err = resolveDockerIp(*ClientName)
+
+		if err != nil {
+			fmt.Println("ERR", err)
+			return
+		}
+	}
+	if *ServerName != "" && *ServerIp == "" {
+		var err error
+		*ServerIp, err = resolveDockerIp(*ServerName)
+
+		if err != nil {
+			fmt.Println("ERR", err)
+			return
+		}
+	}
+
+	// Only use IPs
+	if err := iptablesLink(*ClientIp, *ServerIp, *Port, *Action); err != nil {
+		fmt.Println("ERR", err)
+		return
+	}
+}
+
+func iptablesLink(cip, sip string, p uint, a string) error {
+	var f string
+	if a == "add" {
+		f = "I"
+	} else if a == "delete" {
+		f = "D"
+	}
+
+	fmt.Printf("Linking client %s to server %s on port %d\n", cip, sip, p)
+
+	var (
+		out []byte
+		err error
+	)
+
+	out, err = exec.Command(
+		"/usr/sbin/iptables",
+		"-w", // Wait for xlock
+		"-"+f, "FORWARD",
+		"-s", cip,
+		"-d", sip,
+		"-p", "tcp",
+		"--dport", strconv.FormatUint(uint64(p), 10),
+		"-j", "ACCEPT",
+	).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		return err
+	}
+
+	out, err = exec.Command(
+		"/usr/sbin/iptables",
+		"-w", // Wait for xlock
+		"-"+f, "FORWARD",
+		"-s", sip,
+		"-d", cip,
+		"-p", "tcp",
+		"--sport", strconv.FormatUint(uint64(p), 10),
+		"-j", "ACCEPT",
+	).CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
+		return err
+	}
+
+	return nil
+}
+
+func resolveDockerIp(n string) (string, error) {
+	if Docker == nil {
+		var err error
+		Docker, err = docker.NewClient(*DockerUrl)
+
+		if err != nil {
+			return "", err
+		}
+
+		if err := Docker.Ping(); err != nil {
+			return "", err
+		}
+	}
+
+	var (
+		c   *docker.Container
+		err error
+	)
+
+	for c == nil || c.NetworkSettings.IPAddress == "" {
+		c, err = Docker.InspectContainer(n)
+		if err != nil && !strings.Contains(err.Error(), "No such container") {
+			return "", err
+		}
+
+		fmt.Printf("IP For %s is empty, waiting...\n", n)
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return c.NetworkSettings.IPAddress, nil
+}
